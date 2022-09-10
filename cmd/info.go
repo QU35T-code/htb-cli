a2e36024d3da2737b0ca6842ee33c58dcaf003b2
Kali Linux <qu35t@kali.t2cxqrq2chhullajmklro1mwmc.parx.internal.cloudapp.net>
2022-09-10 22:45:56 +0000

    [ADD] HTB Manager v3.7 (Added Optional Argument blood / Info command)

--- cmd/info.go
+++ cmd/info.go
@@ -10,9 +10,15 @@ var infoCmd = &cobra.Command{
 	Use:   "info",
 	Short: "Displays machine information",
 	Run: func(cmd *cobra.Command, args []string) {
-		machine_id := utils.GetActiveMachineID()
-		machine_id = fmt.Sprintf("%v", machine_id)
-		url := "https://www.hackthebox.com/api/v4/machine/profile/" + machine_id.(string)
+		machine_id := ""
+		if len(args) != 0 {
+			machine_id = utils.SearchMachineIDByName(args[0])
+			machine_id = fmt.Sprintf("%v", machine_id)
+		} else {
+			machine_id_interface := utils.GetActiveMachineID()
+			machine_id = fmt.Sprintf("%v", machine_id_interface)
+		}
+		url := "https://www.hackthebox.com/api/v4/machine/profile/" + machine_id
 		resp := utils.HtbGet(url)
 		info := utils.ParseJsonMessage(resp, "info").(map[string]interface{})
 		fmt.Printf("--- INFO ---\nName : %v\nOS : %v\nPoints : %v\nDifficulty : %v\nIs Completed ? %v", info["name"], info["os"], info["points"], info["difficultyText"], info["isCompleted"])

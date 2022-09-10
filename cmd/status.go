96158b4f37ebdc92592c86f41359b45ddfcda75c
Kali Linux <qu35t@kali.t2cxqrq2chhullajmklro1mwmc.parx.internal.cloudapp.net>
2022-09-10 21:31:12 +0000

    [ADD] HTB Manager v3.5 (Fix bug + Add Active / Retired / Release Machines)

--- cmd/status.go
+++ cmd/status.go
@@ -11,23 +11,37 @@ var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Displays the active machine",
 	Long: ``,
-	Run: func(cmd *cobra.Command, args []string) {		
+	Run: func(cmd *cobra.Command, args []string) {
 		url := "https://www.hackthebox.com/api/v4/machine/active"
 		resp := utils.HtbGet(url)
 		info := utils.ParseJsonMessage(resp, "info")
 		if (info == nil) {
-			fmt.Println("No machine is active")
-			os.Exit(1)
+			url = "https://www.hackthebox.com/api/v4/release_arena/active"
+			resp = utils.HtbGet(url)
+			info = utils.ParseJsonMessage(resp, "info")
+			if (info == nil) {
+				fmt.Println("No machine is active")
+				os.Exit(1)
+			}
+			infomap := info.(map[string]interface{})
+			fmt.Print("ID : ")
+			fmt.Println(infomap["id"])
+			fmt.Print("Name : ")
+			fmt.Println(infomap["name"])
+			fmt.Print("Plan : ")
+			fmt.Println(infomap["type"])
+			fmt.Print("Server : ")
+			fmt.Println(infomap["lab_server"])
 		}
 		infomap := info.(map[string]interface{})
-    	fmt.Print("ID : ")
-    	fmt.Println(infomap["id"])
+		fmt.Print("ID : ")
+		fmt.Println(infomap["id"])
 		fmt.Print("Name : ")
-    	fmt.Println(infomap["name"])
+		fmt.Println(infomap["name"])
 		fmt.Print("Plan : ")
-    	fmt.Println(infomap["type"])
+		fmt.Println(infomap["type"])
 		fmt.Print("Server : ")
-    	fmt.Println(infomap["lab_server"])
+		fmt.Println(infomap["lab_server"])
 	},
 }
 

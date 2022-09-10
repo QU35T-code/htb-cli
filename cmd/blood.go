8bead000fdd96f97b46c9a8bbb3bb23e43767c08
Kali Linux <qu35t@kali.t2cxqrq2chhullajmklro1mwmc.parx.internal.cloudapp.net>
2022-09-10 21:47:54 +0000

    [ADD] HTB Manager v3.6 (Clean Blood command)

--- cmd/blood.go
+++ cmd/blood.go
@@ -4,43 +4,29 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"htb/utils"
-	"os"
 )
 
 var bloodCmd = &cobra.Command{
 	Use:   "blood",
 	Short: "Displays users who have blood the machine",
-	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		machine_id := utils.GetConfigValue("machineid")
 		url := "https://www.hackthebox.com/api/v4/machine/profile/" + machine_id
 		resp := utils.HtbGet(url)
 		info := utils.ParseJsonMessage(resp, "info").(map[string]interface{})
-		if (info["user_owns_count"].(float64) == 0) {
+		fmt.Printf("Machine : %v\n\n", info["name"])
+		if (info["user_owns_count"].(float64) != 0) {
+			infoUserBlood := info["userBlood"].(map[string]interface{})["user"].(map[string]interface{})
+			fmt.Printf("--- USER ---\nName : %v\nTime : %v\n\n", infoUserBlood["name"], info["firstUserBloodTime"])
+		} else {
 			fmt.Println("There is no first blood for the user")
 		}
-    	if (info["root_owns_count"].(float64) == 0) {
+    	if (info["root_owns_count"].(float64) != 0) {
+			infoRootBlood := info["rootBlood"].(map[string]interface{})["user"].(map[string]interface{})
+			fmt.Printf("--- ROOT ---\nName : %v\nTime : %v", infoRootBlood["name"], info["firstRootBloodTime"])
+		} else {
 			fmt.Println("There is no first blood for the root")
 		}
-		if (info["root_owns_count"].(float64) == 0 || info["user_owns_count"].(float64) == 0) {
-			os.Exit(1)
-		}
-		infoUserBlood := info["userBlood"].(map[string]interface{})["user"].(map[string]interface{})
-		infoRootBlood := info["rootBlood"].(map[string]interface{})["user"].(map[string]interface{})
-		fmt.Print("Machine : ")
-    	fmt.Println(info["name"])
-    	fmt.Println("")
-    	fmt.Println("--- USER ---")
-    	fmt.Print("Name : ")
-		fmt.Println(infoUserBlood["name"])
-    	fmt.Print("Time : ")
-    	fmt.Println(info["firstUserBloodTime"])
-		fmt.Println("")
-		fmt.Println("--- Root ---")
-		fmt.Print("Name : ")
-    	fmt.Println(infoRootBlood["name"])
-		fmt.Print("Time : ")
-    	fmt.Println(info["firstRootBloodTime"])
 	},
 }
 
